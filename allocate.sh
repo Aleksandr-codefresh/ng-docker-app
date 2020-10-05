5d15acb1055d3d7fcf87471fcc90a9f6f4ddf22b
Aleksandr-codefresh <45179027+Aleksandr-codefresh@users.noreply.github.com>
2020-10-05 14:58:59 +0300

    Update allocate.sh

--- allocate.sh
+++ allocate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "begin allocating memory..."
-for index in $(seq 1000); do
-    value=$(seq -w -s '' $index $(($index + 1024000000)))
+for index in $(seq 200); do
+    value=$(seq -w -s '' $index $(($index + 10240000000)))
     eval array$index=$value
     free -m
 done
